02ebf953e1c8c4ac76858c03f052391a1e77e5ce
SamanthaZheng-coder <61730851+SamanthaZheng-coder@users.noreply.github.com>
2020-03-03 03:31:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,11 @@
 let slider1;
 let slider2;
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	slider1=createSlider(0,255);	
 	slider2=createSlider(0,255);
 	slider3=createSlider(0,255);
-  
   }
-  
   function draw(){
  background(slider1.value());
  slider2.value();
